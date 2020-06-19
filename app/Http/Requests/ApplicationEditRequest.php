b5eaa97e725f71b54f544887c44e55acc005c583
BablGUM <0mata@mail.ru>
2020-06-19 17:26:30 +0700

    'ver33'

--- app/Http/Requests/ApplicationEditRequest.php
+++ app/Http/Requests/ApplicationEditRequest.php
@@ -22,6 +22,7 @@ class ApplicationEditRequest extends ApiRequest
             'title' => 'required|max:255',
             'description' => 'required|max:2000',
             'budget' => 'required|int|min:1|max:1000000000',
+            'file' => 'mimes:docx,doc,dot,pdf|max:2048',
             'start_date' => 'required|date',
             'end_date' => 'required|date',
         ];
--- app/Http/Requests/ApplicationRequest.php
+++ app/Http/Requests/ApplicationRequest.php
@@ -20,7 +20,7 @@
                 'budget' => 'required|int|min:1|max:1000000000',
                 'start_date' => 'required|date',
                 'end_date' => 'required|date',
-                'file' => 'mimes:docx,doc,dot,pdf|max:5120',
+                'file' => 'mimes:docx,doc,dot,pdf|max:2048',
                 'name_category' => 'required'
 
             ];
