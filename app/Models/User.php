88a9c61f1df760e97d5cd83ba6b8d0746d006a1f
BablGUM <0mata@mail.ru>
2020-07-01 12:12:02 +0700

    'fixedRoles'

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->hasMany('App\Role','id','role_id');
+        return $this->hasMany('App\Models\Role','id','role_id');
     }
 
     /**
